a45c1e10e013ee7775221975c25babfc2e60ba17
shrivastavr45-lab <shrivastavr45@gmail.com>
2026-07-02 16:37:48 +0530

    Time: 0 ms (100%), Space: 10.5 MB (35%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<string>>result;
+    bool isSafe(vector<string>& board,int row,int col,int n){
+        for(int i=0;i<row;i++){
+            if(board[i][col]=='Q') return false;
+        }
+        for(int i=row-1,j=col-1;i>=0 && j>=0 ;i--,j--){
+            if(board[i][j]=='Q') return false;
+        }
+        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
+            if(board[i][j]=='Q') return false;
+        }
+        return true;
+    }
+    void backtrack(vector<string>&board,int row,int n){
+        if(row==n){
+            result.push_back(board);
+            return;
+        }
+        for(int col=0;col<n;col++){
+            if(isSafe(board,row,col,n)){
+                board[row][col]='Q';
+                backtrack(board,row+1,n);
+                board[row][col]='.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string>board(n,string(n,'.'));
+        backtrack(board,0,n);
+        return result;
+    }
+};
