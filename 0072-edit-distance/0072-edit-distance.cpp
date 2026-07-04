4e99c7756f6113166961b588cf4e6c2e97a187d3
shrivastavr45-lab <shrivastavr45@gmail.com>
2026-07-04 17:00:18 +0530

    Time: 7 ms (61.11%), Space: 13.4 MB (29.65%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -3,7 +3,7 @@ public:
     int minDistance(string word1, string word2) {
         int m=word1.size();
         int n=word2.size();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
         for(int i=0;i<=m;i++){
             dp[i][0]=i;
         }
@@ -16,7 +16,7 @@ public:
                     dp[i][j]=dp[i-1][j-1];
                 }
                 else{
-                    dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+                    dp[i][j]=1+min({dp[i][j-1],dp[i-1][j],dp[i-1][j-1]});
                 }
             }
         }
