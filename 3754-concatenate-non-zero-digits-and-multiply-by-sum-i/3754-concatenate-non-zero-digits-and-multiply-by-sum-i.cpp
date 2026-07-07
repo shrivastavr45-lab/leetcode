091c2b9ee3f1c05c2bf53ee556b53feffb718a3e
shrivastavr45-lab <shrivastavr45@gmail.com>
2026-07-07 16:52:46 +0530

    Time: 0 ms (100%), Space: 9 MB (74.01%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     long long sumAndMultiply(int n) {
         string s=to_string(n);
-        long long x=0;
+        long long ans=0;
         int sum=0;
         for(char c:s){
             if(c!='0'){
                 int t=c-'0';
-                x=x*10+t;
+                ans=ans*10+t;
                 sum+=t;
             }
         }
-        return x*sum;
+        return ans*sum;
     }
 };
