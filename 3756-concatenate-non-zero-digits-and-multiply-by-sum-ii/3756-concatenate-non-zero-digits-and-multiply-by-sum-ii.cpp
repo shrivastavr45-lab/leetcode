eef8902bcd7cd741d91baa923214d00ba09ddd05
shrivastavr45-lab <shrivastavr45@gmail.com>
2026-07-08 10:14:29 +0530

    Time: 79 ms (29.02%), Space: 169.3 MB (26.43%) - LeetHub

---
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    static const int MOD = 1e9 + 7;
+
+    vector<int> solveQueries(string s, vector<vector<int>>& queries) {
+        int n = s.size();
+
+        vector<int> pos, digit;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] != '0') {
+                pos.push_back(i);
+                digit.push_back(s[i] - '0');
+            }
+        }
+
+        int k = digit.size();
+
+        vector<long long> pow10(k + 1, 1);
+        for (int i = 1; i <= k; i++)
+            pow10[i] = (pow10[i - 1] * 10) % MOD;
+
+        vector<long long> prefVal(k + 1, 0);
+        vector<int> prefSum(k + 1, 0);
+
+        for (int i = 0; i < k; i++) {
+            prefVal[i + 1] = (prefVal[i] * 10 + digit[i]) % MOD;
+            prefSum[i + 1] = prefSum[i] + digit[i];
+        }
+
+        vector<int> ans;
+
+        for (auto &q : queries) {
+            int l = q[0], r = q[1];
+
+            int L = lower_bound(pos.begin(), pos.end(), l) - pos.begin();
+            int R = upper_bound(pos.begin(), pos.end(), r) - pos.begin() - 1;
+
+            if (L > R) {
+                ans.push_back(0);
+                continue;
+            }
+
+            int len = R - L + 1;
+
+            long long x = (prefVal[R + 1] -
+                           prefVal[L] * pow10[len] % MOD + MOD) % MOD;
+
+            long long sum = prefSum[R + 1] - prefSum[L];
+
+            ans.push_back((x * sum) % MOD);
+        }
+
+        return ans;
+    }
+
+    vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+        return solveQueries(s, queries);
+    }
+};
