b549341ef79016ac94861745e9bf74b8da30f661
shrivastavr45-lab <shrivastavr45@gmail.com>
2026-07-17 11:09:57 +0530

    Time: 49 ms (50.67%), Space: 129.8 MB (66.67%) - LeetHub

---
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
+        int mx=*max_element(nums.begin(),nums.end());
+        vector<int>freq(mx+1,0);
+        for(int x:nums){
+            freq[x]++;
+        }
+        vector<long long>exact(mx+1,0);
+        for(int g=mx;g>=1;g--){
+            long long cnt=0;
+            for(int multiple=g;multiple<=mx;multiple+=g){
+                cnt+=freq[multiple];
+            }
+            exact[g]=cnt*(cnt-1)/2;
+            for(int multiple=2*g;multiple<=mx;multiple+=g)
+                exact[g]-=exact[multiple];
+        }
+        vector<long long>prefix(mx+1,0);
+        for(int i=1;i<=mx;i++){
+            prefix[i]=prefix[i-1]+exact[i];
+        }
+        vector<int>ans;
+        for(long long q:queries){
+            int g=lower_bound(prefix.begin()+1,prefix.end(),q+1)-prefix.begin();
+            ans.push_back(g);
+        }
+        return ans;
+    }
+};
