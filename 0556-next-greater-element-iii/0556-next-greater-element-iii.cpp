24803e51bd6a0af1f872a57f29068c38bf1d3ec9
shrivastavr45-lab <shrivastavr45@gmail.com>
2026-07-17 18:55:12 +0530

    Time: 0 ms (100%), Space: 7.7 MB (85.17%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string s=to_string(n);
+        int len=s.size();
+        int i=len-2;
+        while(i>=0 && s[i]>=s[i+1]){
+            i--;
+        }
+        if(i<0){
+            return -1;
+        }
+        int j=len-1;
+        while(s[j]<=s[i]){
+            j--;
+        }
+        swap(s[i],s[j]);
+        reverse(s.begin()+i+1,s.end());
+        long long ans=stoll(s);
+        if(ans>INT_MAX)
+            return -1;
+        return (int)ans;
+    }
+};
